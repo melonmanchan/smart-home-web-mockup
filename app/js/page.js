9b53dcf65960bce58fd3ad70889c6033bcfc0d70
Matti Jokitulppo <melonmanchan@gmail.com>
2016-11-23 09:57:03 +0200

    Add light switch effect

--- app/js/page.js
+++ app/js/page.js
@@ -3,5 +3,6 @@ $(document).ready(function () {
         var val = this.value;
         var elem = $(this).data('room')
         $('.' + elem).html(val + '%');
+        $('.' + elem + '-card').css({ boxShadow: '0px 0px ' + val / 3 + 'px #fff' });
     });
 });
